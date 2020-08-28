50d2d8bfa5bd787e9d460c8e6d44c7525e0e004e
aufalmarom <aufalmarom23@gmail.com>
2020-08-28 08:01:53 +0700

    routes web

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::group(['prefix'=>'v1'], function(){
-    Route::get('/api/nakama', 'NakamaController@index')->name('getall');
-    Route::get('/api/nakama/{id}', 'NakamaController@show')->name('getbyid');
-    Route::post('/api/nakama', 'NakamaController@store')->name('create');
-    Route::put('/api/nakama/{id}', 'NakamaController@update')->name('update');
-    Route::delete('/api/nakama/{id}', 'NakamaController@destroy')->name('delete');
-});
